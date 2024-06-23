4702479cdfc34ae497b51522f19ee9f2a2355a4c
Ishigami100 <yamahaya5151@icloud.com>
2024-06-24 02:34:36 +0900

    せっていを入れる

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -4,7 +4,7 @@ import Poster from '../pages/Poster'
 
 const appRoutes = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/poster" element={<Poster />} />
